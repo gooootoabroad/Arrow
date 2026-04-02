8836d2d024d746a4a4792369c5a3f329aab4e3f0
pengpongpong <2695968851@qq.com>
2026-04-02 20:38:40 +0800

    [feat] 优化画图性能

--- assets/src/global/GameConfig.ts
+++ assets/src/global/GameConfig.ts
@@ -2,5 +2,8 @@
 export class GameConfig {
     // 单位size
     static readonly UNIT_SIZE: number = 40;
-
+    // 箭头多大
+    static readonly arrowLineWidth: number = GameConfig.UNIT_SIZE * 0.5;
+    // 箭头头部多大
+    static readonly arrowHeadSize: number = GameConfig.UNIT_SIZE * 0.7;
 }
